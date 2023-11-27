67e2d1a399f625b76dc8c4fb98a1ecd9dbfc0206
shahriyor60 <96121714+shahriyor60@users.noreply.github.com>
2023-11-27 09:35:26 +0000

    The final commit of the first project in python

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+print("Welcome to the Band Name Generator")
+print("What is the name of city where you grew up?")
+city = input()
+print("What is your pet's name")
+pet = input()
+
+print(f"Your band name could be {city} {pet}")
